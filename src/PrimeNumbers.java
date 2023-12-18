c5326925e59f4f1c34825271bf6cea81c37bd7a7
AbdiJavad <Mirjavadabdi@gmail.com>
2023-12-18 19:42:17 +0330

    Updated,Changed

--- src/PrimeNumbers.java
+++ src/PrimeNumbers.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class PrimeNumbers {
+    
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Find all prime numbers <= n, enter n: ");
